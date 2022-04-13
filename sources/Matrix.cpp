1f98b684056644fdc5cf104b8594b9c058ceb53d
AmirGill <amirg00@walla.com>
2022-04-13 12:42:46 +0300

    Fix implementation for input, also add documentation.

--- sources/Matrix.cpp
+++ sources/Matrix.cpp
@@ -14,8 +14,9 @@ using namespace zich;
 //==================== Methods ====================/
 /**************************************************/
 
+// Constructor
 Matrix::Matrix(vector<double> matrix, int row, int col)
-    : _matrix(move(matrix)), _row(row), _col(col){
+: _matrix(move(matrix)), _row(row), _col(col){
     if (_row <= 0 || _col <= 0) {
         throw invalid_argument("Row and col are zero/negative!");
     }
@@ -24,6 +25,7 @@ Matrix::Matrix(vector<double> matrix, int row, int col)
     }
 }
 
+// Plus operation for a matrix: mat1 + mat2
 Matrix Matrix::operator+(const Matrix &other) {
     if (_row != other._row || _col != other._col) {
         throw invalid_argument("Matrices dimension isn't the same!");
@@ -34,9 +36,7 @@ Matrix Matrix::operator+(const Matrix &other) {
     return add;
 }
 
-/**
- * Unary operator: +Matrix
- */
+// Unary operator: +Matrix
 Matrix Matrix::operator+() {
     vector<double> add_mat((unsigned long) (_row * _col), 0);
     for (unsigned long i = 0; i < _matrix.size(); ++i) {add_mat.at(i) = _matrix[i];}
@@ -44,48 +44,41 @@ Matrix Matrix::operator+() {
     return plus;
 }
 
+// Insertion for a matrix: mat1 += mat2
 Matrix& Matrix::operator+=(const Matrix &other) {
     if (_row != other._row || _col != other._col) {
         throw invalid_argument("Matrices dimension isn't the same!");
     }
-
-    for (unsigned long i = 0; i < _matrix.size(); ++i) {
-        _matrix.at(i) += other._matrix[i];
-    }
+    for (unsigned long i = 0; i < _matrix.size(); ++i) {_matrix.at(i) += other._matrix[i];}
     return *this;
 }
 
-/**
- * Prefix method: ++Matrix
- */
+
+// Prefix method: ++Matrix
 Matrix& Matrix::operator++() {
     for (double &element : _matrix) {++element;}
     return *this;
 }
 
-/**
- * Postfix method: Matrix++
- */
+// Postfix method: Matrix++
 Matrix Matrix::operator++(int) {
     Matrix cpy{_matrix, _row, _col}; /* Copy of current matrix*/
     ++*this;
     return cpy;
 }
 
+// Minus operation for a matrix: mat1 - mat2
 Matrix Matrix::operator-(const Matrix &other) {
     if (_row != other._row || _col != other._col) {
         throw invalid_argument("Matrices dimension isn't the same!");
     }
-
     vector<double> add_mat((unsigned long) (_row * _col), 0);
-
-    for (unsigned long i = 0; i < _matrix.size(); ++i) {
-        add_mat.at(i) = _matrix[i] - other._matrix[i];
-    }
+    for (unsigned long i = 0; i < _matrix.size(); ++i) {add_mat.at(i) = _matrix[i] - other._matrix[i];}
     Matrix add{add_mat, _row, _col};
     return add;
 }
 
+// Unary operator for minus: -Matrix
 Matrix Matrix::operator-() {
     vector<double> add_mat((unsigned long) (_row * _col), 0);
 
@@ -109,17 +102,13 @@ Matrix& Matrix::operator-=(const Matrix &other) {
     return *this;
 }
 
-/**
- * Prefix method: --Matrix
- */
+// Prefix method: --Matrix
 Matrix& Matrix::operator--() {
     for (double &element : _matrix) {--element;}
     return *this;
 }
 
-/**
- * Postfix method: Matrix--
- */
+//Postfix method: Matrix--
 Matrix Matrix::operator--(int) {
     Matrix cpy{_matrix, _row, _col}; /* Copy of current matrix*/
     --*this;
@@ -127,7 +116,7 @@ Matrix Matrix::operator--(int) {
 }
 
 
-
+// Matrices multiplication: mat1 * mat2
 Matrix Matrix::operator*(const Matrix &other){
     if (_col != other._row) {
         throw invalid_argument("Cannot multiply by definition.!");
@@ -148,11 +137,13 @@ Matrix Matrix::operator*(const Matrix &other){
     return add;
 }
 
+// mat1 *= mat2
 Matrix& Matrix::operator*=(const Matrix &other){
     *this = *this * other;
     return *this;
 }
 
+// mat1 *= scalar
 Matrix& Matrix::operator*=(double scalar) {
     *this = *this * scalar;
     return *this;
@@ -174,6 +165,8 @@ bool Matrix::operator<=(const Matrix &other) const{
     return Matrix::compareByOperator(*this, other, "<=");
 }
 
+// Method checks if two matrices are equivalent,
+// meaning that they have the same elements.
 bool Matrix::operator==(const Matrix &other) const{
     if (_row != other._row || _col != other._col) {
         throw invalid_argument("Matrices dimension isn't the same!");
@@ -194,13 +187,7 @@ bool Matrix::operator!=(const Matrix &other) const{
 //================== Friend Functions =============/
 /**************************************************/
 
-/**
- * Method compares by the following operator relations (">", ">=", "<", "<=")
- * @param a
- * @param b
- * @param Operator
- * @return
- */
+//Method compares by the following operator relations (">", ">=", "<", "<=").
 bool Matrix::compareByOperator(const Matrix &a, const Matrix &b, const string &Operator) {
     if (a._row != b._row || a._col != b._col) {
         throw invalid_argument("Matrices dimension isn't the same!");
@@ -219,18 +206,14 @@ bool Matrix::compareByOperator(const Matrix &a, const Matrix &b, const string &O
     return relation;
 }
 
-/**
- * OUTPUT METHOD
- * @param os - output stream reference
- * @param matrix
- * @return
- */
+// Output method for a matrix, by a given matrix object.
 ostream& zich::operator<<(ostream& os, const Matrix& matrix)
 {
+    /*Validation check */
     if (matrix._matrix.size() != matrix._row * matrix._col){
         throw invalid_argument("Vector size doesn't match given matrix dimension.");
     }
-    if (matrix._col == 1){
+    if (matrix._col == 1){ /*Matrix has only one column*/
         for (unsigned long i = 0; i < matrix._matrix.size(); ++i) {
             os << '[' << matrix._matrix[i] << ']';
             if (i != matrix._matrix.size() - 1){os << '\n';}
@@ -255,78 +238,87 @@ ostream& zich::operator<<(ostream& os, const Matrix& matrix)
     return os;
 }
 
-
-
-// [1 1 1], [3 4 5], [3 3 2]
-
+// Function gets a string and checks validation to be a matrix.
+// Note: the check in which a matrix doesn't match given col and row properties isn't included here.
 void checkIfValid(string mat){
-    string delim = ",";
-    size_t pos;
-    size_t cnt = 0;
-    while ((pos = mat.find (delim)) != string::npos)
-    {
-        string token = mat.substr(0, pos); // store the substring
-        if ((token[0] != '['|| token[token.length()-1] != ']') && (cnt == 0)){
-            throw invalid_argument("Invalid matrix.");
+    char delim = ',';
+    string token;
+    for (unsigned long i = 0, comma_cnt = 0, lbr_cnt = 0, rbr_cnt = 0; i < mat.length(); ++i) {
+        token += mat[i];
+        if (mat[i] == delim){ /*Reached a comma*/
+            comma_cnt++;
+            token = token.substr(0, token.length()-1);
+            if ((token[0] != '['|| token[token.length()-1] != ']') && (comma_cnt == 1)){
+                throw invalid_argument("Invalid matrix!");
+            }
+            if ((token.substr(0,2) != " [" || token[token.length()-1] != ']') && (comma_cnt != 1)){
+                throw invalid_argument("Invalid matrix!");
+            }
+            if (lbr_cnt != 1 || rbr_cnt != 1){ /*Only one left and right square brackets are allowed*/
+                throw invalid_argument("Only one left and right square brackets are allowed!");
+            }
+            rbr_cnt = 0; lbr_cnt = 0;
+            token = "";
         }
-        else if ((mat.substr(0,2) != " [" || token[token.length()-1] != ']') && (cnt != 0)){
-            throw invalid_argument("Invalid matrix.");
+        /*Count left bracket, and check that there isn't a space right after*/
+        else if (mat[i] == '['){
+            lbr_cnt++;
+            if (mat[i+1] == ' '){
+                throw invalid_argument("Invalid matrix.");
+            }
         }
-        mat.erase(0, pos + delim.length());  /* erase() function store the current positon and move to next token. */
-        cnt++;
+        /*Count right bracket, and check that there isn't a space before*/
+        else if (mat[i] == ']'){
+            rbr_cnt++;
+            if (mat[i-1] == ' '){
+                throw invalid_argument("Invalid matrix.");
+            }
+        }
     }
+
 }
 
-/**
- * INPUT METHOD
- *
- * GET A MATRIX WITH THE FOLLOWING FORM: [1 3 5], [3 4 5], [6 7 8]
- *
- * @param is
- * @param matrix
- * @return
- */
+// Input function: GET A MATRIX WITH THE FOLLOWING FORM: [1 3 5], [3 4 5], [6 7 8] for example.
+// Function inserts the data to matrix's properties.
 istream& zich::operator>>(istream& is, Matrix& matrix)
 {
     string checkMat;
     getline(is, checkMat);
     checkIfValid(checkMat);
 
-    istringstream iss{checkMat};
-    string mat;
+    /*Get the matrix's properties with string stream to split*/
     vector<double> mat_vec;
+    stringstream ss{checkMat};
+    string split;
+    size_t col_cnt = 0;
+    size_t row_cnt = 0;
 
-    int row = 1; int col = 0; int col_cnt = 0;
-    do{
-        iss >> mat;
-        char last_ch = mat[mat.length() - 1];
-
-        /* Remove unnecessary chars*/
-        std::erase(mat, '[');
-        std::erase(mat, ']');
-
-        if(last_ch!= ',' && col == 0){
-            mat_vec.push_back(stod(mat));
-            col_cnt++;
+    /*Split first by a comma - the string is valid so yields a valid result*/
+    while (getline(ss, split, ',')) {
+        if (row_cnt != 0) {split = split.substr(1, split.length()-1);}
+        cout << split << endl;
+        std::erase(split, '['); std::erase(split, ']');
+        stringstream ss_split{split};
+        string num;
+        /*Split with a space to get the float numbers*/
+        while (getline(ss_split, num, ' ')) {
+            mat_vec.push_back(stod(num));
+            if (row_cnt == 0){col_cnt++;}
         }
-        else if (last_ch != ',' && col != 0){
-            mat_vec.push_back(stod(mat));
-        }
-        else if (last_ch == ','){
-            mat_vec.push_back(stod(mat.substr(0, mat.length() - 1)));
-            col = col_cnt + 1;
-            row++;
-        }
+        row_cnt++;
     }
-    while (iss.peek() != '\n');
 
-    if (row == 1) {col = col_cnt;}
-    matrix._row = row;
-    matrix._col = col;
+    if(mat_vec.size() != row_cnt*col_cnt){
+        throw invalid_argument("Matrix doesn't match its given row and column properties!");
+    }
+    matrix._row = row_cnt;
+    matrix._col = col_cnt;
     matrix._matrix = mat_vec;
     return is;
 }
 
+// Multiplication of a matrix by scalar function on right side.
+// I.e.: mat * scalar
 Matrix zich::operator*(const Matrix &matrix, double scalar) {
     vector<double> add_mat((unsigned long) (matrix._row * matrix._col), 0);
     for (unsigned long i = 0; i < matrix._matrix.size(); ++i) {add_mat.at(i) = matrix._matrix[i] * scalar;}
@@ -334,8 +326,8 @@ Matrix zich::operator*(const Matrix &matrix, double scalar) {
     return mul;
 }
 
+// Multiplication of a matrix by scalar function on left side.
+// I.e.: mat * scalar
 Matrix zich::operator*(double scalar, const Matrix &matrix) {
     return matrix * scalar;
 }
-
-
